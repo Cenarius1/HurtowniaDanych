891f912f4ad330c8d462f771b87c21dd95b8801e
Cenarius <entityjordanpark@gmail.com>
2018-10-21 01:28:46 +0100

    Setup solution .NET 4.7.2 Core 2.2

---
+++ HurtowniaDanych/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HurtowniaDanych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Read();
+        }
+    }
+}
