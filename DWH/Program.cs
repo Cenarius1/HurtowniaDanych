0a7f0a1be2ecfc63e5c974895b7a21bfa0b4424b
Adam Oramus <adam.oramus@ericsson.com>
2018-12-02 20:38:57 +0100

    Take parameter from command line

--- DWH/Program.cs
+++ DWH/Program.cs
@@ -3,6 +3,11 @@
 namespace DWH {
     class Program {
         static void Main(string[] args) {
+            //TODO: Parametrize ETLProcessManager
+            if (args.Length > 0) {
+                var manufacturer = args[0];
+            }
+
             var etlManager = new ETLProcessManager();
 
             etlManager.Launch();
