021e80d426fac98f3971be95b85b6ad18bcee7a4
Adam Oramus <adam.oramus@ericsson.com>
2018-12-02 15:05:04 +0100

    Added SelectAll(); and Find(<prediction>) function for querying data

--- DWH.Storage/CarDetailsRepository.cs
+++ DWH.Storage/CarDetailsRepository.cs
@@ -1,5 +1,9 @@
 ﻿using DWH.Domain;
 using DWH.Storage.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace DWH.Storage {
     public class CarDetailsRepository : IGenericRepository<LoadCarDetail> {
@@ -13,6 +17,16 @@ namespace DWH.Storage {
             context.CarDetails.Add(dbo);
 
             return dbo;
+        }
+
+        public List<LoadCarDetail> SelectAll() {
+            var carDetails = context.CarDetails.ToList();
+            return carDetails;
+        }
+
+        public List<LoadCarDetail> Find(Expression<Func<LoadCarDetail, bool>> prediction) {
+            var carDetails = context.CarDetails.Where(prediction).ToList();
+            return carDetails;
         }
 
         public void SaveChanges() {
--- DWH.Storage/Interface/IGenericRepository.cs
+++ DWH.Storage/Interface/IGenericRepository.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace DWH.Storage.Interface {
     public interface IGenericRepository<TDomain> {
         TDomain Insert(TDomain dbo);
+        List<TDomain> SelectAll();
+        List<TDomain> Find(Expression<Func<TDomain, bool>> prediction);
         void SaveChanges();
     }
 }
